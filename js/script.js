c7621eb6a4f1c47be237cc5b713f426575090853
BillThai <bthai@hawk.iit.edu>
2019-11-08 08:49:33 -0600

    fixed java script for order page, styled form page, added correct addresses to tutor and order sheets

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 function init()
 {
-  var fname = document.getElementById("first_name").value;
-  var lname = document.getElementById("last_name").value;
-  var email = document.getElementById("Email").value;
-  var phone = document.getElementById("Phone_Number").value;
-  var belt = document.getElementById("belt_length").value;
-  var design = document.getElementById("design_layout").value;
+  var name = document.getElementById("fullname").value;
+  var email = document.getElementById("email").value;
   
-  alert("Thank You " +fname+ "" +lname+ " for your order of " +design+ " at the length of " +belt+ " we will send you at message to "+email+ " or phone you at " +phone+ ".");
+  alert("Thank You " +name+ " for your order we will send you at message to "+email+"");
 
 }
 
+/*  var belt = document.getElementById("belt_length").value;
+  var design = document.getElementById("design_layout").value;
+  alert("Thank You " +fullname+ " for your order of " +design+ " at the length of " +belt+ " we will send you at message to "+email+ " or phone you at " +phone+ ".");
+*/
