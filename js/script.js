9acf4b9a2fa381075db3284bf99594c2aef71f0b
phaynes2 <46732008+phaynes2@users.noreply.github.com>
2019-11-13 18:49:03 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,12 @@ function init()
   alert("Thank You " +name+ " for your order we will send you at message to "+email+"");
 
 }
-
+ function validate() {
+	var x = document.forms["myForm"]["fname"].value;
+	if(x == "") {
+		alert("your must be filled out!");
+		return false;
+	}
 /*  var belt = document.getElementById("belt_length").value;
   var design = document.getElementById("design_layout").value;
   alert("Thank You " +fullname+ " for your order of " +design+ " at the length of " +belt+ " we will send you at message to "+email+ " or phone you at " +phone+ ".");
