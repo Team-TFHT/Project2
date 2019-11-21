be981403258cd46a3d54811310d3c0fe16e62e02
BillThai <bthai@hawk.iit.edu>
2019-11-20 21:27:21 -0600

    Edit Javascript

--- js/script.js
+++ js/script.js
@@ -15,10 +15,8 @@ function validate() {
   }
 
 function copyDate() {
-  var cpyrt = document.getElementById(“copyright”)
-  if (cpyrt) {
-     cpyrt.firstChild.nodeValue = (new Date()).getFullYear();
-  }
+  var cpyrt = document.getElementById(“copyright”);
+  cpyrt = dt.getYear();
 }
 
 window.onload = copyDate;
